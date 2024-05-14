7c6e85d18673067cfa0b8e688d144d4950ad3a9c
AntoineRai <antoine.raimand@gmail.com>
2024-05-14 09:38:53 +0200

    chore: Remove duplicate import of cors module

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const cors = require('cors');
-
 require('dotenv').config();
 
 const { expressjwt: jwt } = require("express-jwt");
