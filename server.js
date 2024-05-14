c416c75daecefc3ac8af1b72071e150ec0529f2d
TzRPheonix <tzrpheonix@gmail.com>
2024-05-14 15:48:06 +0200

    rateLimit augmented

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const port = 3000;
 // Limiter le nombre de requêtes par IP
 app.use(rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  limit: 20, // Limit each IP to 20 requests per `window` (here, per 1 minute).
+  limit: 100, // Limit each IP to 20 requests per `window` (here, per 1 minute).
   standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 }));
