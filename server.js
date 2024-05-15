72add25f7ffa4769103c7be818930f93b0b4fbe2
MathisSenicourt <mathiscience@hotmail.fr>
2024-05-15 09:58:12 +0200

    feat: allow specifying port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const POIRouteur = require("./routes/POIRoutes");
 const UserRouteur = require("./routes/userRoutes");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Limiter le nombre de requêtes par IP
 app.use(rateLimit({
